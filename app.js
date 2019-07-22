ed91b2e5dd586cb49f1cfac75c192ca8b1f601c7
pedro_bras <pedro.c.bras@gmail.com>
2019-07-22 13:45:20 +0200

    adds weather api and geolocation, also adds function to set DOM elements from API

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 console.log('Hello World')
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  let temperatureDescription = document.querySelector('.temperature-description');
+  let temperatureDegree = document.querySelector('.temperature-degrees');
+  let locationTimezone = document.querySelector('.location-timezone');
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      
+      const proxy = `https://cors-anywhere.herokuapp.com/`;
+      const api = `${proxy}https://api.darksky.net/forecast/4a0fd97e4b1e4f1f043d7388cb0244a1/${lat},${long}`;
+
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+          const { temperature, summary } = data.currently;
+          // Set DOM Elements from API
+          temperatureDegree.textContent = temperature;
+          temperatureDescription.textContent = summary;
+        })
+    })
+  } else {
+    h1.textContent = "Please enable your location to view the weather!"
+  }
+})
