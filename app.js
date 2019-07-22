04de3f3129d3a07b93a367a03cc943d0f180bd14
pedro_bras <pedro.c.bras@gmail.com>
2019-07-22 14:43:27 +0200

    adds catch to fetch fuction and formula to convert farenheit to celcius

--- app.js
+++ app.js
@@ -21,13 +21,17 @@ window.addEventListener('load', () => {
         .then(response => {
           return response.json();
         })
+        .catch(error => {
+          console.log(error);
+        })
         .then(data => {
           const { temperature, summary, icon } = data.currently;
           // Set DOM Elements from API
           temperatureDegree.textContent = temperature;
           temperatureDescription.textContent = summary;
           locationTimezone.textContent = data.timezone;
-
+          // Formula to conver farenheit to celsius
+          let celsius = (temperature - 32) * (5 / 9);
           // Set Icon
           setIcons(icon, document.querySelector(".icon"));
 
@@ -35,8 +39,10 @@ window.addEventListener('load', () => {
           temperatureSection.addEventListener('click', () =>{
             if(temperatureSpan.textContent === "F"){
               temperatureSpan.textContent = "C";
+              temperatureDegree.textContent = Math.floor(celsius);
             } else {
               temperatureSpan.textContent = "F";
+              temperatureDegree.textContent = temperature;
             }
           });
         });
